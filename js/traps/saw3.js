a42d0c82050254d301a77ef732189e268efd2b20
nmc1998 <nmc7198@gmail.com>
2017-08-10 22:38:39 +0700

    Add horizontal movement saw

---
+++ js/traps/saw3.js
@@ -0,0 +1,24 @@
+class Saw3 {
+  constructor(x, y, x1, x2) {
+    this.sprite = PrisonBreak.trapGroup.create(x, y, 'saw1');
+    this.sprite.update = this.update.bind(this);
+
+    this.sprite.body.collideWorldBounds = true;
+    this.sprite.anchor = new Phaser.Point(0.5, 0.5);
+    this.sprite.body.kinematic = true;
+    this.SAW_SPEED = 135;
+    this.DIRECT;
+    this.x1 = x1;
+    this.x2 = x2;
+    this.sprite.body.setCircle(this.sprite.width / 2);
+  }
+
+  update() {
+    this.sprite.body.rotation += 30;
+    if (this.sprite.x < this.x1) this.DIRECT = false;
+    if (this.sprite.x > this.x2) this.DIRECT = true;
+
+    if (this.DIRECT) this.sprite.body.velocity.x = -this.SAW_SPEED;
+    else this.sprite.body.velocity.x = this.SAW_SPEED;
+  }
+}
