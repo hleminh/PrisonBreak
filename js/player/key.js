ae7318f5ccb086ee13cd228c61e435d08b012c36
DESKTOP-F3C8V4H\dell <ots.hunter@gmail.com>
2017-08-10 15:56:48 +0700

    done stage 2 with key and stuff

--- js/player/key.js
+++ js/player/key.js
@@ -2,5 +2,6 @@ class Key {
   constructor(x, y) {
     this.sprite = PrisonBreak.keyGroup.create(x, y, 'key');
     this.sprite.anchor = new Phaser.Point(0.5, 0.5);
+    this.sprite.body.kinematic = true;
   }
 }
