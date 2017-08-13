13c83dc22a0b57688764df06ac087fb42f63b69e
DESKTOP-82BCULB\Abuu <duongnk.techkids@gmail.com>
2017-08-13 18:47:26 +0700

    edited win screen

--- js/zmain/load.js
+++ js/zmain/load.js
@@ -28,6 +28,7 @@ var loadState = {
     PrisonBreak.game.load.image('background_lock', 'assets/bgs/lock_pg.png');
     PrisonBreak.game.load.image('background_unlock', 'assets/bgs/unlock_bg.png');
     PrisonBreak.game.load.image('background_menuLvl', 'assets/bgs/menuLvl_bg.png');
+    PrisonBreak.game.load.image('prisonHouse', 'assets/bgs/prisonhouse.png');
     PrisonBreak.game.load.audio('death','assets/sound/Player_Hit_1.wav');
     PrisonBreak.game.load.audio('saw','assets/sound/Item_13.wav');
     PrisonBreak.game.load.audio('coin','assets/sound/Coins.wav');
--- js/zmain/win.js
+++ js/zmain/win.js
@@ -1,11 +1,16 @@
 var winState = {
   create: function() {
-    var nameLabel = PrisonBreak.game.add.text(80, 80, 'You won', {
-      font: "50px Arial",
+    PrisonBreak.game.stage.backgroundColor = '#00000';
+    PrisonBreak.player = PrisonBreak.game.add.sprite(250, PrisonBreak.configs.GAME_HEIGHT - 90, 'player' );
+    PrisonBreak.player.scale.setTo(2, 2);
+    PrisonBreak.prison = PrisonBreak.game.add.sprite(576, 407.8, 'prisonHouse');
+    PrisonBreak.prison.anchor = new Phaser.Point(0.5, 0.5);
+    var nameLabel = PrisonBreak.game.add.text(370, 80, 'YOU WON!!!', {
+      font: "70px Arial",
       fill: "#ffffff"
     });
 
-    var startLabel = PrisonBreak.game.add.text(80, PrisonBreak.configs.GAME_HEIGHT - 80, 'Press Enter to restart', {
+    var startLabel = PrisonBreak.game.add.text(450, PrisonBreak.configs.GAME_HEIGHT - 650, 'Press Enter to restart', {
       font: "25px Arial",
       fill: "#ffffff"
     });
@@ -16,6 +21,10 @@ var winState = {
   start: function(){
     PrisonBreak.deathCount = 0;
     PrisonBreak.game.state.start('stage1');
-
+  },
+  update(){
+    if(PrisonBreak.player.position.x <= PrisonBreak.configs.GAME_WIDTH - 210){
+      PrisonBreak.player.position.x += 5;
+    }
   }
 }
